51f279060d5803e44da6c6685fb69391ba79c58c
Coja <jv@wingman.ai>
2025-08-27 11:37:25 +0200

    [Fish] binds update

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,7 @@ function fish_user_key_bindings
     bind -M default ctrl-e end-of-line
     bind -M insert ctrl-space accept-autosuggestion
     bind -M insert ctrl-g 'git diff' repaint
+
+    bind -M insert ctrl-o execute yazi
+    bind -M insert ctrl-l clear-screen ls repaint
 end
