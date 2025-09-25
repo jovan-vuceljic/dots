6303af5edb58c5adfb23fdb1bcd5c6486fc836bc
Coja <jv@wingman.ai>
2025-09-25 11:23:57 +0200

    [Nvim] pylsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,5 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "python_lsp_server", "eslint" }
+-- local servers = { "html", "cssls", "ts_ls", "python_lsp_server", "eslint" }
+local servers = { "html", "cssls", "ts_ls", "pylsp", "eslint" }
 vim.lsp.enable(servers)
