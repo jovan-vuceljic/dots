ae57cb7e9a2141d825339ed1117360d8b1081a6a
Coja <jv@wingman.ai>
2026-07-23 11:51:57 +0200

    [Fish] nvidia

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set -gx EDITOR nvim
+set -Ux LIBVA_DRIVER_NAME nvidia
 set -gx PAGER less
 set -Ux MANPAGER "nvim +Man!"
 set -x NEWT_COLORS 'root=black,black;window=black,black;border=white,black;listbox=white,black;label=blue,black;checkbox=red,black;title=green,black;button=white,red;actsellistbox=white,red;actlistbox=white,gray;compactbutton=white,gray;actcheckbox=white,blue;entry=lightgray,black;textbox=blue,black' nmtui
